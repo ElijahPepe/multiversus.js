b9f8509afbc4f20cda95d1b849b08cf6e19cc99e
Elijah Conners <business@elijahpepe.com>
2022-07-30 16:18:25 -0700

    chore: add new endpoints, better error handling
    * Add the searchByUsername and getProfile endpoints
    * Better handling for errors
    * Add types to getLeaderboard, 1v1 and 2v2
    * Connect NPM to GitHub
    
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ export class Client {
 		this.userAgent = userAgent || 'Hydra-Cpp/1.132.0';
 	}
 
-	getLeaderboard() {
-		const data = fetch(base + '/leaderboards/2v2/show', {
+	searchByUsername(username, limit) {
+		if (!limit) {
+			limit = 25;
+		}
+		if (!username) {
+			throw new Error('A query must be provided.')
+		}
+		const data = fetch(base + `/profiles/search_queries/get-by-username/run?username=${username}&limit=${limit}`, {
 			headers: {
 				'x-hydra-access-token': this.accessToken,
 				'x-hydra-api-key': this.apiKey,
@@ -19,6 +25,63 @@ export class Client {
 				'x-hydra-user-agent': this.userAgent
 			}
 		}).then(res => {
+			if (res.status === 403) {
+				throw new Error('Invalid Access Token')
+			} else if (res.status === 401) {
+				throw new Error('User session kicked')
+			}
+			return res.text();
+		}).then(json => {
+			return json;
+		})
+
+		return data
+	}
+
+	getProfile(id) {
+		if (!id) {
+			throw new Error('A user ID must be provided.')
+		}
+		const data = fetch(base + `/profiles/${id}`, {
+			headers: {
+				'x-hydra-access-token': this.accessToken,
+				'x-hydra-api-key': this.apiKey,
+				'x-hydra-client-id': this.clientId,
+				'x-hydra-user-agent': this.userAgent
+			}
+		}).then(res => {
+			if (res.status === 404) {
+				throw new Error(`'${id}' is not a valid account_id`)
+			} else if (res.status === 403) {
+				throw new Error('Invalid Access Token')
+			} else if (res.status === 401) {
+				throw new Error('User session kicked')
+			}
+			return res.text();
+		}).then(json => {
+			return json;
+		})
+
+		return data;
+	}
+
+	getLeaderboard(type) {
+		if (type !== '2v2' && type !== '1v1') {
+			throw new Error('Leaderboard type must be 1v1 or 2v2.')
+		}
+		const data = fetch(base + `/leaderboards/${type}/show`, {
+			headers: {
+				'x-hydra-access-token': this.accessToken,
+				'x-hydra-api-key': this.apiKey,
+				'x-hydra-client-id': this.clientId,
+				'x-hydra-user-agent': this.userAgent
+			}
+		}).then(res => {
+			if (res.status === 403) {
+				throw new Error('Invalid Access Token')
+			} else if (res.status === 401) {
+				throw new Error('User session kicked')
+			}
 			return res.text();
 		}).then(json => {
 			return json;
