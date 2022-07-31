8fd876f53a708d0617673cd2123a1f845e93c631
Elijah Conners <business@elijahpepe.com>
2022-07-30 20:37:34 -0700

    test: add unit test for getProfile()
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

---
+++ test/getProfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import assert from 'node:assert';
+import { accessToken, apiKey, clientId } from './auth.js'
+import { Client } from '../index.js';
+
+const client = new Client(accessToken, apiKey, clientId);
+
+const userId = '62e471bc5f77e966a384a570'
+const userData = await client.getProfile(userId);
+try {
+	console.log(userId)
+	assert.strictEqual(userData.account_id, userId);
+} catch (err) {
+	console.error(error);
+}
