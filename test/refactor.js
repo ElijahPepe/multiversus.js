b9ce716b770cb1ad1442325ded60665330161f9a
Henrik-3 <henrikmertens3@gmail.com>
2022-08-02 19:04:01 +0200

    New request handler + auth fix

---
+++ test/refactor.js
@@ -0,0 +1,10 @@
+import assert from 'node:assert';
+import { clientId, username, password } from './auth.js';
+import { Client } from '../src/index.js';
+
+const client = new Client(null, clientId);
+await client.login(username, password);
+
+const userId = '62e471bc5f77e966a384a570';
+const userData = await client.getAccount(userId);
+console.log(userData);
