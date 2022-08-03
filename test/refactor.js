74a2e14413b1ee884e3337e501b51fe8e6798d49
Elijah Conners <business@elijahpepe.com>
2022-08-02 18:31:15 -0700

    test: add proper tests to refactor.js
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

--- test/refactor.js
+++ test/refactor.js
@@ -7,4 +7,9 @@ await client.login(username, password);
 
 const userId = '62e471bc5f77e966a384a570';
 const userData = await client.getAccount(userId);
-console.log(userData);
+try {
+	console.log(userId);
+	assert.strictEqual(userData.account_id, userId);
+} catch (err) {
+	console.error(err);
+}
