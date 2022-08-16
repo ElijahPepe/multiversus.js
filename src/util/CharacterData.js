6b7388d295db66a346234f00a4acd74a79b2611a
Elijah Conners <business@elijahpepe.com>
2022-08-15 21:32:38 -0700

    chore: restructure project
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

---
+++ src/util/CharacterData.js
@@ -0,0 +1,111 @@
+class CharacterData {
+	static Shaggy = {
+		id: 'character_shaggy',
+		displayName: 'Shaggy',
+		aliases: [],
+	};
+
+	static WonderWoman = {
+		id: 'character_wonder_woman',
+		displayName: 'Wonder Woman',
+		aliases: [],
+	};
+
+	static Batman = {
+		id: 'character_batman',
+		displayName: 'Batman',
+		aliases: [],
+	};
+
+	static Superman = {
+		id: 'character_superman',
+		displayName: 'Superman',
+		aliases: [],
+	};
+
+	static Taz = {
+		id: 'character_taz',
+		displayName: 'Taz',
+		aliases: [],
+	};
+
+	static IronGiant = {
+		id: 'character_C017',
+		displayName: 'Iron Giant',
+		aliases: [],
+	};
+
+	static Garnet = {
+		id: 'character_garnet',
+		displayName: 'Garnet',
+		aliases: [],
+	};
+
+	static StevenUniverse = {
+		id: 'character_steven',
+		displayName: 'Steven Universe',
+		aliases: [],
+	};
+
+	static Jake = {
+		id: 'character_jake',
+		displayName: 'Jake the Dog',
+		aliases: ['Jake'],
+	};
+
+	static Reindog = {
+		id: 'character_creature',
+		displayName: 'Reindog',
+		aliases: [],
+	};
+
+	static Finn = {
+		id: 'character_finn',
+		displayName: 'Finn the Human',
+		aliases: ['Finn'],
+	};
+
+	static Velma = {
+		id: 'character_velma',
+		displayName: 'Velma',
+		aliases: [],
+	};
+
+	static AryaStark = {
+		id: 'character_arya',
+		displayName: 'Arya Stark',
+		aliases: [],
+	};
+
+	static BugsBunny = {
+		id: 'character_bugs_bunny',
+		displayName: 'Bugs Bunny',
+		aliases: [],
+	};
+
+	static HarleyQuinn = {
+		id: 'character_harleyquinn',
+		displayName: 'Harley Quinn',
+		aliases: [],
+	};
+
+	static TomAndJerry = {
+		id: 'character_tom_and_jerry',
+		displayName: 'Tom and Jerry',
+		aliases: [],
+	};
+
+	static LeBronJames = {
+		id: 'character_c16',
+		displayName: 'LeBron James',
+		aliases: [],
+	};
+
+	static RickSanchez = {
+		id: 'character_c020',
+		displayName: 'Rick Sanchez',
+		aliases: ['Rick'],
+	};
+}
+
+module.exports = CharacterData;
