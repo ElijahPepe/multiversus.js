8bfae2e9f8d063ecaba4a3530257fc09ebdc3ba5
ioamra <iom.monchau@gmail.com>
2022-10-03 04:47:07 +0200

    leaderboard page

--- src/managers/LeaderboardManager.js
+++ src/managers/LeaderboardManager.js
@@ -16,13 +16,14 @@ class LeaderboardManager extends BaseManager {
 	/**
 	 * Obtains the leaderboard from MultiVersus
 	 * @param {LeaderboardTypes} type The type of leaderboard to fetch
-	 * @returns {Promise<any>}
+	 * @param {number?} page The page
+	 * @returns {Promise<Object>}
 	 */
-	async fetch(type) {
+	 async fetch(type, page = 1) {
 		if (type !== '2v2' && type !== '1v1') {
 			throw new Error('Leaderboard type must be 1v1 or 2v2.');
 		}
-		const data = await this.client.rest.get(Routes.leaderboard(type));
+		const data = await this.client.rest.get(Routes.leaderboard(type, page));
 		return data;
 	}
 
@@ -30,16 +31,17 @@ class LeaderboardManager extends BaseManager {
 	 * Obtains a character leaderboard from MultiVersus
 	 * @param {LeaderboardTypes} type The type of leaderboard to fetch
 	 * @param {string} character The character leaderboard to fetch
-	 * @returns {Promise<any>}
+	 * @param {number?} page The page
+	 * @returns {Promise<Object>}
 	 */
-	async fetchCharacter(type, character) {
+	async fetchCharacter(type, character, page = 1) {
 		if (type !== '2v2' && type !== '1v1') {
 			throw new Error('Leaderboard type must be 1v1 or 2v2.');
 		}
 		if (!character) {
 			throw new Error('A character must be provided.');
 		}
-		const data = await this.client.rest.get(Routes.leaderboardCharacter(type, character));
+		const data = await this.client.rest.get(Routes.leaderboardCharacter(type, character, page));
 		return data;
 	}
 
